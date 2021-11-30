b9e843430bed129306b7cd5e00dededbe7f6cd2c
Yomakota <teoperitore@outlook.com>
2021-11-30 12:14:33 +0100

    jsnack-2-end

--- jsnack 2/js/script.js
+++ jsnack 2/js/script.js
@@ -1,1 +1,28 @@
-// Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+// Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari;
+
+// Crea un array di numeri interi che si riempie coi numeri chiesti all'utente;
+let numbers = [];
+let sum = 0;
+
+for (let i = 0; i < 10; i++) {
+
+    let number = parseInt(prompt(`insert number`));
+    numbers.push(number); //per riempire array vuoto;
+
+   
+    if (i % 2 == 0) {
+
+        console.log('even position', numbers[i]);
+    } 
+
+    else {
+        //  la somma di tutti i numeri che sono in posizione dispari;
+        sum += numbers[i];
+
+        console.log('odd position', numbers[i]);
+    }
+}
+
+const h1 = document.querySelector('h1');
+h1.innerHTML ='The sum is: ' + sum;
+console.log('odd position numbers sum is: ' + sum);
